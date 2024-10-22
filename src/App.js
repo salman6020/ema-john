496718c20716bf1e4a0fc596f8954596b400b3aa
salman6020 <salmanmiaje6020@gmail.com>
2024-10-22 17:26:11 +0600

    no

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Goo work salman Go ahed
         </a>
       </header>
     </div>
